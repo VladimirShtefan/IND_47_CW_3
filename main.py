eba6422cf8f18e6074c69299d7fc45e685ec0151
VladimirShtefan <shtefanvladimir14@gmail.com>
2024-06-08 19:28:01 +0700

    all good

---
+++ main.py
@@ -0,0 +1,15 @@
+from settings import OPERATIONS_PATH, COUNT_OPERATIONS
+from src.utils import load_operations, load_operations_instances, get_executed_operations, sort_operations_by_date
+
+
+def main():
+    operations1 = load_operations(OPERATIONS_PATH)
+    operations_instances = load_operations_instances(operations1)
+    ex_op = get_executed_operations(operations_instances)
+    sort_op = sort_operations_by_date(ex_op)[:COUNT_OPERATIONS]
+    for op in sort_op:
+        print(op)
+
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+OPERATIONS_PATH = Path(__file__).parent / "src/data/operations.json"
+COUNT_OPERATIONS = 5
